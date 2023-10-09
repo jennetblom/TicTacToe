104cf9b8b70922ad3a723c5afb60561b22a12dad
Jennet blom <jeboblom20@hotmail.com>
2023-10-09 14:31:40 +0200

    Added singleplayer and multiplayer

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
@@ -7,6 +8,7 @@ public class Main {
     static Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) {
+
 
         //choose between 1 player or two players
         System.out.println();
@@ -17,15 +19,26 @@ public class Main {
 
 
         Gameboard gameboard = new Gameboard();
+        System.out.println("How do you want to play?");
+        System.out.println("1. Singleplayer");
+        System.out.println("2. Multiplayer ");
+        int chooseGameMode= scan.nextInt();
 
-
-
-        ArrayList<Player> playerList = multiPlayer();
-        playGame(playerList, gameboard);
+        switch(chooseGameMode){
+            case 1:
+                ArrayList<Player> playerListWithCpu = singlePlayer();
+                singlePlayerGame(playerListWithCpu, gameboard);
+                break;
+            case 2:
+                ArrayList<Player> playerList = multiPlayer();
+                multiPlayerGame(playerList,gameboard);
+                break;
+        }
 
     }
 
     private static ArrayList<Player> multiPlayer() {
+        scan.nextLine();
         ArrayList<Player> playerList = new ArrayList<>();
 
         System.out.println("Write out the name of first player");
@@ -40,7 +53,25 @@ public class Main {
 
         return playerList;
     }
+    private static ArrayList<Player>singlePlayer(){
+        scan.nextLine();
+        ArrayList<Player>playerListWithCpu=new ArrayList<>();
+        System.out.println("Write out the name of first player");
+        String playerName1 = scan.nextLine();
+        Player p1=new Player(playerName1,'X');
+        Player CPU=new Player("Computer", 'O');
+        System.out.println("Player "+ p1.getGamePiece()+": " + playerName1);
+        System.out.println("Player "+ CPU.getGamePiece()+": " + CPU.getName());
+        playerListWithCpu.add(p1);
+        playerListWithCpu.add(CPU);
 
+        return playerListWithCpu;
+    }
+    public static int askComputerWhichSquare(){
+        Random random = new Random();
+        int randomSquare = random.nextInt(9)+1;
+        return randomSquare;
+    }
     public static int askWhichSquare() {
 
         System.out.println("Which square do you want to choose");
@@ -48,7 +79,7 @@ public class Main {
         return whichSquare;
     }
 
-    public static void playGame(ArrayList<Player> playerList, Gameboard gameboard) {
+    public static void multiPlayerGame(ArrayList<Player> playerList, Gameboard gameboard) {
 
         while (true) {
             for (Player player : playerList) {
@@ -57,21 +88,63 @@ public class Main {
                 int whichSquare = askWhichSquare();
                 gameboard.setPiece(whichSquare);
                 gameboard.drawBoard();
-                gameboard.checkIfThreeInArow(player.getGamePiece());
-                gameboard.ifThereIsATie();
 
                 if(gameboard.checkIfThreeInArow(player.getGamePiece())){
-                    System.out.println("CONGRATZZZ YOU WON!!! "+ player.getName());
+                    System.out.println("CONGRATZZZ YOU WON!!! "+ "Player "+player.getName());
+                    System.out.println();
                     player.increaseScore();
-                    System.out.println("You have " +player.getScore() +" in score");
                     return;
                 }
                 if(gameboard.ifThereIsATie()){
-                    System.out.println("No one won! You've got a tie");
+                    System.out.println("No one won! You've got a tie:(");
+                    System.out.println();
                     return;
                 }
             }
         }
+    }
+    public static void singlePlayerGame(ArrayList<Player>playersWithCpu, Gameboard gameboard){
 
+        int whichSquare;
+        while (true) {
+            System.out.println();
+            System.out.println("Player " + playersWithCpu.get(0).getName() + " - it's your turn to put " + playersWithCpu.get(0).getGamePiece());
+            gameboard.switchPlayer(playersWithCpu.get(0));
+            whichSquare=askWhichSquare();
+            gameboard.setPiece(whichSquare);
+            gameboard.drawBoard();
+            if(gameboard.checkIfThreeInArow(playersWithCpu.get(0).getGamePiece())){
+                System.out.println("CONGRATZZZ YOU WON!!! "+ "Player "+playersWithCpu.get(0).getName());
+                System.out.println();
+                playersWithCpu.get(0).increaseScore();
+                return;
+            }
+            System.out.println();
+            System.out.println("Player " + playersWithCpu.get(1).getName() + " - it's your turn to put " + playersWithCpu.get(1).getGamePiece());
+            gameboard.switchPlayer(playersWithCpu.get(1));
+            whichSquare=askComputerWhichSquare();
+            gameboard.setPiece(whichSquare);
+            gameboard.drawBoard();
+            if(gameboard.checkIfThreeInArow(playersWithCpu.get(1).getGamePiece())){
+                System.out.println("CONGRATZZZ YOU WON!!! "+ "Player "+playersWithCpu.get(1).getName());
+                System.out.println();
+                playersWithCpu.get(1).increaseScore();
+                return;
+            }
+            if(gameboard.ifThereIsATie()){
+                System.out.println("No one won! You've got a tie:(");
+                System.out.println();
+                return;
+            }
+
+        }
+    }
+
+    public static void showScore(ArrayList<Player> playerList, Gameboard gameboard){
+        for(Player player:playerList){
+            //  System.out.println();
+            System.out.println("Player " + player.getName()+ " - score: "+player.getScore());
+            System.out.println();
+        }
     }
 }
